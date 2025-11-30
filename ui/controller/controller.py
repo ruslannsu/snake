918c8523b3ee253b7d75e0d1f04d1e608a0e6631
ruslan <ruslanone57@gmail.com>
2025-11-30 15:47:13 +0700

    add: listener, sender

--- ui/controller/controller.py
+++ ui/controller/controller.py
@@ -1,15 +1,22 @@
 from ui.model.model import Model
 from ui.view.view import View
 from snake.roles.master import Master
-
+from snake.game.listener import GameListener
+from snake.game.sender import GameSender
 from event_bus.event_bus import event_bus
 
 class Controller:
-    def __init__(self, view: View, model: Model, master_role: Master) -> None:
+    def __init__(self, view: View, model: Model, master_role: Master, game_listener: GameListener, game_sender: GameSender) -> None:
         self._view = view
         self._model = model
         self._master_role = master_role
         self._game_role = None
+        self._game_listener = game_listener
+        game_listener.start()
+
+        self._game_sender = game_sender
+        self._game_sender.start()
+
         event_bus.subscribe(event_name="start_game", handler=self._set_master_role)
         
     def _set_master_role(self):
